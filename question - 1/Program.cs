16df03030bbae2c09e2deb7479bb6ebc41368753
AAYUSH540 <fadia.aayush@gmail.com>
2022-01-31 19:47:11 +0530

    my project

---
+++ question - 1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace dgh
+{
+    class DoTheMath
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("\nPlease enter the numerator? ");
+            int num = int.Parse(Console.ReadLine());
+            Console.Write("Please enter the denominator? ");
+            int den = int.Parse(Console.ReadLine());
+
+            int rem = num%den;
+            int quo = num/den;
+
+            Console.WriteLine($"\nInteger division result = {quo} with a remainder {rem}");
+
+            double quo2 = (double)(num/den);
+            Console.WriteLine($"\nFloating point division result = {quo2}");
+
+            Console.WriteLine($"\nThe result as a mixed fraction is {quo} {rem}/{den} ");
+        }
+    }
+}
---
+++ question - 2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("\nEnter String : ");
+            string str = Console.ReadLine();
+
+            Console.WriteLine($"{str} -> {str.Length}");
+
+            Console.Write("\nEnter Sentence : ");
+            string sen = Console.ReadLine();
+
+            if(sen.EndsWith("."))
+            {
+                Console.WriteLine("-> Declarative Sentence");
+            }
+            else if(sen.EndsWith("?"))
+            {
+                Console.WriteLine("-> Interrogatory Sentence");
+            }
+            else if(sen.EndsWith("!"))
+            {
+                Console.WriteLine("-> Exclamation Sentence");
+            }
+            else
+            {
+                Console.WriteLine("-> Not a Sentence");
+            }
+
+            Console.Write("\nEnter Your Name : ");
+            string name = Console.ReadLine();
+
+            string [] words = name.Split(" ");
+            Console.Write($"Name : ");
+
+            for(int i = words.Length - 1; i >=0 ; i--)
+            {
+                Console.Write(words[i]);
+                if(i>0){Console.Write(" , ");}
+            }
+        }
+    }
+}
